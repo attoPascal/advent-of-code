3195f45b8da2f94ab0ba296ffb1e6846042c55d7
Pascal Attwenger <pascal.attwenger@gmail.com>
2022-12-10 14:08:30 +0100

    Day 10

--- 2022/day09.py
+++ 2022/day09.py
@@ -24,13 +24,13 @@ def simulate(rope_length, directions):
     for direction, steps in directions:
         for _ in range(steps):
             rope[0] = move(rope[0], direction)
-            for knot in range(len(rope)-1):
-                rope[knot+1] = follow(rope[knot+1], rope[knot])
+            for knot in range(1, len(rope)):
+                rope[knot] = follow(rope[knot], rope[knot-1])
             visited.add(rope[-1])
 
-    return len(visited)
+    return visited
 
 directions = [(line[0], int(line[2:])) for line in lines]
 
-print("Part 1:", simulate(2,  directions))
-print("Part 2:", simulate(10, directions))
+print("Part 1:", len(simulate(2,  directions)))
+print("Part 2:", len(simulate(10, directions)))
